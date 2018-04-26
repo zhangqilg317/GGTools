822e4ed61e7ef2df053a92344e78ce5524a7e7c2
zhangqi <by_zhangqi@163.com>
2018-04-26 09:40:15 +0800

    导航定义
    导航定义
    扩展方法

---
+++ GGTools/导航定义/GGNavigationController.h
@@ -0,0 +1,21 @@
+//
+//  GGNavigationController.h
+//  GGTools
+//
+//  Created by 张奇 on 2018/4/26.
+//  Copyright © 2018年 com.ebest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ 自定义导航基类
+ 用于实现对整体导航器的控制
+ 例如：右滑手势的开关、导航标题的定制等等
+ 关联文档 :
+ iOS右滑返回手势深度全解和最佳实施方案
+ https://juejin.im/post/5adeda3051882567336a5dc9?utm_source=gold_browser_extension
+ */
+@interface GGNavigationController : UINavigationController
+
+@end
---
+++ GGTools/导航定义/GGNavigationController.m
@@ -0,0 +1,29 @@
+//
+//  GGNavigationController.m
+//  GGTools
+//
+//  Created by 张奇 on 2018/4/26.
+//  Copyright © 2018年 com.ebest. All rights reserved.
+//
+
+#import "GGNavigationController.h"
+
+@interface GGNavigationController ()
+
+@end
+
+@implementation GGNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
